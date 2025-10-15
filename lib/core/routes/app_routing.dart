97c6020a01ee028c667b497fda1c6df334e3d3cd
fatmaatef304 <fatimaatef990@gmailcom>
2025-10-15 15:17:43 +0300

    Onboarding Screen done

--- lib/core/routes/app_routing.dart
+++ lib/core/routes/app_routing.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pet_finder_app/core/routes/routes.dart';
+import 'package:pet_finder_app/features/onboarding/presentation/screens/onboarding_screen.dart';
 import 'package:pet_finder_app/features/splash/presentation/screens/splash_screen.dart';
 
 class AppRouting {
@@ -7,6 +8,9 @@ class AppRouting {
     switch (routesSettings.name) {
       case Routes.splashScreen:
         return MaterialPageRoute(builder: (_) => SplashScreen());
+      case Routes.onboardingScreen:
+        return MaterialPageRoute(builder: (_) => OnboardingScreen());
     }
+    return null;
   }
 }
