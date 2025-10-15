268497a8c9343cdb8f237c3307b74d36c9f16166
fatmaatef304 <fatimaatef990@gmailcom>
2025-10-15 23:38:25 +0300

    Fav Screen UI done

--- lib/core/routes/app_routing.dart
+++ lib/core/routes/app_routing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pet_finder_app/core/routes/routes.dart';
 import 'package:pet_finder_app/features/details/presentation/screens/details_screen.dart';
+import 'package:pet_finder_app/features/favourite/presentation/screens/fav_screen.dart';
 import 'package:pet_finder_app/features/home/presentation/screens/home_screen.dart';
 import 'package:pet_finder_app/features/onboarding/presentation/screens/onboarding_screen.dart';
 import 'package:pet_finder_app/features/splash/presentation/screens/splash_screen.dart';
@@ -14,8 +15,10 @@ class AppRouting {
         return MaterialPageRoute(builder: (_) => OnboardingScreen());
       case Routes.homeScreen:
         return MaterialPageRoute(builder: (_) => HomeScreen());
-       case Routes.detailsScreen:
+      case Routes.detailsScreen:
         return MaterialPageRoute(builder: (_) => DetailsScreen());
+      case Routes.favScreen:
+        return MaterialPageRoute(builder: (_) => FavScreen());
     }
     return null;
   }
